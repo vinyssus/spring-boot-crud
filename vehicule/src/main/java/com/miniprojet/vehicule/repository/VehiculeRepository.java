df3270004652b771dbf7778665c959d94b463b65
unknown <vinyssusrodolphe@gmail.com>
2022-12-17 13:42:55 +0100

    query selector

--- vehicule/src/main/java/com/miniprojet/vehicule/repository/VehiculeRepository.java
+++ vehicule/src/main/java/com/miniprojet/vehicule/repository/VehiculeRepository.java
@@ -1,8 +1,12 @@
 package com.miniprojet.vehicule.repository;
 
+
+
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.miniprojet.vehicule.entities.Vehicule;
@@ -10,6 +14,7 @@ import com.miniprojet.vehicule.entities.Vehicule;
 @Repository
 public interface VehiculeRepository extends JpaRepository<Vehicule, Integer>{
 
-	
-	List<Vehicule> findByMarqueContains(String mc);
+	@Query(value="select v from Vehicule v where v.marque like %:mc%")
+	public List<Vehicule> findByMarqueContains(@Param("mc") String mc);
+	//List<Vehicule> findByMarqueContains(String mc);
 }
